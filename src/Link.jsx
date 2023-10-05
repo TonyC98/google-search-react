85516f6a2b9ffae69be3220bac1785a5d29f89fd
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-05 13:14:17 +0700

    Links render
    Added if statement in the links render, website was crushing when there were no links to display

--- src/Link.jsx
+++ src/Link.jsx
@@ -1,14 +1,18 @@
 function Link({ data }) {
-  return (
-    <ul>
-      {data.links.map((link, index) => (
-        <li key={index}>
-          <a href={link.url} target="_blank" rel="noreferrer">
-            {link.title}
-          </a>
-        </li>
-      ))}
-    </ul>
-  )
+  if (data.links) {
+    return (
+      <ul>
+        {data.links.map((link, index) => (
+          <li key={index}>
+            <a href={link.url} target="_blank" rel="noreferrer">
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    )
+  } else {
+    return null
+  }
 }
 export default Link
